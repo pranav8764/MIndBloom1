1d895c3d2abff2b01a5221ecab4ab5e1a49bef2b
Pranav Singh Rajoria <pranavrajoria1@gmail.com>
2026-05-17 15:45:11 +0530

    modified the folder structure

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -31,19 +31,4 @@ export default defineConfig([
       "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
     },
   },
-  {
-    files: ["server/**/*.{js}"],
-    extends: [js.configs.recommended],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.node,
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-      },
-    },
-    rules: {
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
-    },
-  },
 ]);
